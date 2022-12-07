201dfeadc28e05ca803b97356915a75b09d22057
Nagendrasomisetti <119730093+Nagendrasomisetti@users.noreply.github.com>
2022-12-07 18:49:11 +0530

    #include<stdio.h> int tar(int p1,int p2,int p3,int p4) {     if(p1>=10 and p2>=10 and p3>=10 and p4>=10)     {         return 1;     }     else     {         return 2;     } } int main() {     int x,p1,p2,p3,p4;     scanf("%d%d%d%d",&p1,&p2,&p3,&p4);     x=tar(p1,p2,p3,p4);     if(x==1)     {         printf("YES");     }     else     {         printf("NO");     } }

--- Target.c
+++ Target.c
@@ -1,9 +1,21 @@
 #include<stdio.h>
+int tar(int p1,int p2,int p3,int p4)
+{
+    if(p1>=10 and p2>=10 and p3>=10 and p4>=10)
+    {
+        return 1;
+    }
+    else
+    {
+        return 2;
+    }
+}
 int main()
 {
-    int p1,p2,p3,p4;
+    int x,p1,p2,p3,p4;
     scanf("%d%d%d%d",&p1,&p2,&p3,&p4);
-    if(p1>=10 and p2>=10 and p3>=10 and p4>=10)
+    x=tar(p1,p2,p3,p4);
+    if(x==1)
     {
         printf("YES");
     }
